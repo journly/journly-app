ac306e7e764749a713b63dc4c73de2bb0d7046f2
renchiey <renchieyang@gmail.com>
2025-04-13 22:41:16 +1000

    working migration solution

---
+++ server/migrations/V1__users.rs
@@ -0,0 +1,15 @@
+use barrel::{types, Migration, backend::Pg};
+
+pub fn migration() -> String {
+    let mut m = Migration::new();
+
+    m.create_table("users", |t| {
+        t.add_column("id", types::serial().primary(true));
+        t.add_column("email", types::varchar(200).nullable(false));
+        t.add_column("first_name", types::varchar(200).nullable(false));
+        t.add_column("last_name", types::varchar(200).nullable(false));
+        t.add_column("username", types::varchar(50).nullable(false).unique(true));
+    });
+
+    m.make::<Pg>()
+}
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,24 +1,86 @@
+use std::ops::DerefMut;
+
 use actix_web::{ get, post, web, App, HttpResponse, HttpServer, Responder };
+use confik::{ Configuration, EnvSource };
+use database::{config::ExampleConfig, db, models::{self, User}};
+use errors::MyError;
+use deadpool_postgres::{Client, Pool};
+use dotenvy::dotenv;
+use tokio_postgres::NoTls;
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+
+mod errors;
+mod database;
+
+
+
+mod embedded {
+    use refinery::embed_migrations;
+    embed_migrations!("migrations");
 }
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
+#[get("/users")]
+async fn get_users(dp_pool: web::Data<Pool>) -> impl Responder {
+
+    let client: Client = dp_pool.get().await.map_err(MyError::PoolError).unwrap();
+
+    let result = db::get_users(&client).await;
+
+    match result {
+        Ok(users) => HttpResponse::Ok().json(users),
+        Err(_) => HttpResponse::InternalServerError().into()
+    }
 }
 
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
+#[post("/users")]
+async fn add_user(user: web::Json<models::User>, db_pool: web::Data<Pool>) -> impl Responder {
+    let user_info: User = user.into_inner();
+
+    let result = db_pool.get().await.map_err(MyError::PoolError);
+
+    match result {
+        Ok(client) => {
+            let result = db::add_user(&client, user_info).await;
+
+            match result {
+                Ok(new_user) => HttpResponse::Ok().json(new_user),
+                Err(_) => HttpResponse::InternalServerError().into()
+            }
+        },
+        Err(_) => HttpResponse::InternalServerError().into()
+    }
 }
+
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new().service(hello).service(echo).route("/hey", web::get().to(manual_hello))
+    dotenv().ok();
+
+    let config = ExampleConfig::builder()
+        .override_with(EnvSource::new())
+        .try_build()
+        .unwrap();
+
+    let pool = config.pg.create_pool(None, NoTls).expect("Failed to connect to DB");
+
+    let mut conn = pool.get().await.unwrap();
+
+    let client = conn.deref_mut().deref_mut();
+
+    let report = embedded::migrations::runner().run_async(client).await.expect("Migration failed");
+
+    println!("Migration report: {report:?}");
+
+    let server = HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(pool.clone()))
+            .service(get_users)
+            .service(add_user)
     })
-        .bind(("127.0.0.1", 8080))?
-        .run().await
+        .bind((config.server_addr.clone(), config.dev_port.clone()))?
+        .run();
+
+    println!("Server running on {}:{}", config.server_addr, config.dev_port);
+
+    server.await
 }
