92c1d2b7f433e65b7e030264e0e7710b976f2d6b
renchiey <renchieyang@gmail.com>
2025-05-11 00:58:18 +1000

    removed schema.sql

--- server/schema.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE IF NOT EXISTS 'journaly_db';
-USE 'journaly_db';
-
-CREATE USER IF NOT EXISTS 'journaly'@'localhost' IDENTIFIED BY 'user';
-GRANT SELECT, INSERT, UPDATE, DELETE ON 'journaly_db'.* TO 'journaly'@'localhost';
