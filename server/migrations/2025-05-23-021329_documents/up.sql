f653fe587dfd52eebdc2ab90a23cf6add1a56094
renchiey <renchieyang@gmail.com>
2025-06-01 00:15:30 +1000

    changed expense up migration

--- server/migrations/2025-05-23-021329_documents/up.sql
+++ server/migrations/2025-05-23-021329_documents/up.sql
@@ -5,6 +5,7 @@ CREATE TABLE documents (
   document_url TEXT NOT NULL,
   file_hash TEXT NOT NULL,
   file_type TEXT NOT NULL,
+  file_size BIGINT NOT NULL,
   created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
   FOREIGN KEY(trip_id)
