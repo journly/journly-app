3e2f8fe7899feaeb33141c49595b98997022c010
renchiey <renchieyang@gmail.com>
2025-07-13 13:33:27 +1000

    removed unused provider from default layout

--- client/src/components/DefaultLayout.tsx
+++ client/src/components/DefaultLayout.tsx
@@ -1,17 +1,14 @@
 import { Outlet } from 'react-router-dom';
 import { MenuBar } from './menu/MenuBar';
 import { Box } from '@mui/material';
-import { MenuProvider } from '../providers/MenuProvider';
 
 export default function DefaultLayout() {
   return (
-    <MenuProvider>
-      <Box className="flex h-screen w-full bg-gray-50">
-        <MenuBar />
-        <Box className="flex-1 overflow-auto">
-          <Outlet />
-        </Box>
+    <Box className="flex h-screen w-full bg-gray-50">
+      <MenuBar />
+      <Box className="flex-1 overflow-auto">
+        <Outlet />
       </Box>
-    </MenuProvider>
+    </Box>
   );
 }
